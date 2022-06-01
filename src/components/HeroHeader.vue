0606ec81e0878bb1af029135a611ed72bddc39f6
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-06-02 00:32:51 +0800

    Added mutahar OwO

--- src/components/HeroHeader.vue
+++ src/components/HeroHeader.vue
@@ -3,19 +3,30 @@
     <span>Welcome to</span>
     <h2>SCAMMERINO</h2>
     <span>What do you wanna do today?</span>
+    <img src="../assets/mutahar-funny.jpg" alt="" srcset="">
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .hero-header {
   width: 100%;
-  height: 10rem;
+  height: 12rem;
   display: flex;
   color: white;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background: url('../assets/veri_angri_scammer.jpg') no-repeat center center;
-  background-size: cover;
+  position: relative;
+  background: rgba(0,0,0,0.5);
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 12rem;
+    z-index: -1;
+    object-fit: cover;
+  }
 }
 </style>
